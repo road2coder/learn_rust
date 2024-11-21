c7aa06b845c5dc6d9e8d3fdd22160fa493f19e06
r2coder <r2coder@163.com>
2024-11-21 20:06:24 +0800

    initial commit

---
+++ 01_getting_started/1.3_hello_cargo/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // cargo new proj: create a new project
+    // cargo new proj --vcs none: create new project without creating a git rep
+    // cargo run: run project
+    // cargo build: compile without optimizations
+    // cargo build --release: compile with optimizations
+    println!("Hello, cargo!");
+}
