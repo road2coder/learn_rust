311f675e525421e20c5c3d9dc7343dc274539b46
r2coder <r2coder@163.com>
2024-11-22 11:23:03 +0800

    guessing_name: handle invalid input and print more friendly

--- 02_guessing_game/src/main.rs
+++ 02_guessing_game/src/main.rs
@@ -3,19 +3,37 @@ use std::{cmp::Ordering, io};
 
 fn main() {
     println!("Guessing the number!");
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret_number is {secret_number}");
+    let mut min = 1;
+    let mut max = 100;
+    let secret_number = rand::thread_rng().gen_range(min..max);
 
     loop {
         let mut guess = String::new();
-        println!("Please input your guess.");
+        println!("Please input your guess({min} to {max}).");
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("_");
+                continue;
+            }
+        };
+
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small"),
-            Ordering::Greater => println!("Too big"),
+            Ordering::Less => {
+                println!("Too small");
+                if secret_number > min {
+                    min = guess
+                }
+            }
+            Ordering::Greater => {
+                if guess < max {
+                    max = guess
+                }
+            }
             Ordering::Equal => {
                 println!("You win!");
                 break;
