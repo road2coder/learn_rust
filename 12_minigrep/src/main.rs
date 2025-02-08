d1d0e7f6752a646195b7ba677f844585207603a4
r2coder <r2coder@163.com>
2025-02-08 14:38:04 +0800

    minigrep: fix: parse config error

--- 12_minigrep/src/main.rs
+++ 12_minigrep/src/main.rs
@@ -3,7 +3,9 @@ use std::{env, process};
 use minigrep::{run, Config};
 
 fn main() {
-    let config = Config::build(env::args()).unwrap_or_else(|err| {
+    let mut args = env::args();
+    args.next();
+    let config = Config::build(args).unwrap_or_else(|err| {
         eprintln!("Error parsing arguments: {err}");
         process::exit(1);
     });
